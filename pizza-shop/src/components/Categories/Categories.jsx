78e976df60814d5b3502fc4c98bc9a126f79f8c6
unknown <alexsandrvanin@gmail.com>
2020-06-29 14:28:24 +0300

    add onCkickChange

--- pizza-shop/src/components/Categories/Categories.jsx
+++ pizza-shop/src/components/Categories/Categories.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Categories = () => {
+const Categories = ({ items }) => {
+
+    const [activeItem, setActiveItem] = useState(1)
+
     return(
         <div className="categories">
             <ul>
                 <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    items.map( (item, index) => <li 
+                        key = {`${item}_${index}`} 
+                        className = {`${activeItem === index ? 'active' : ''}`}
+                        onClick = {() => setActiveItem(index)}
+                    >{item}</li> )
+                }
             </ul>
         </div>
     )
